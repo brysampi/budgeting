ec0af1a0989d5f7393e6ef6929bc94a5487f9a19
BELL (Bryan) <bryansampi08@gmail.com>
2025-06-19 16:20:36 +0300

    update: Vite Config Chunck Limit Warning

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Set to 1000 KB or higher as needed
+  },
 })
