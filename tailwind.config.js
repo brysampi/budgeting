c4e1645c8ba7211ee721d193a50b442cbfa814a2
BELL (Bryan) <bryansampi08@gmail.com>
2025-07-21 17:08:13 +0300

    add: Install Tailwind Css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Must include all template files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
